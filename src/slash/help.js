a617dbb28ea068156ebab6579874ce7565f4f6ee
Thomas Etter <tometter03@gmail.com>
2022-12-26 22:06:01 -0500

    FIrst slash command

--- src/slash/help.js
+++ src/slash/help.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder, EmbedBuilder }= require("discord.js");
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName("about")
+		.setName("help")
 		.setDescription("Sends an ephemeral message with info about the bot."),
 	async execute(client, interaction) {
         const replyEmbed = new EmbedBuilder()
---
+++ src/slash/index.js
@@ -0,0 +1,4 @@
+const about = require("./about");
+const help = require("./help");
+
+exports.commands = [about, help];
