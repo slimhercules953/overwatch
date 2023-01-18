79da7ba4528a5c1b04d8be3ff0c2296821be677a
Thomas Etter <tometter03@gmail.com>
2023-01-18 11:33:19 -0500

    Updated half of support stats

--- src/slash/stats.js
+++ src/slash/stats.js
@@ -13,7 +13,7 @@ module.exports = {
         .setDescription('Info about the character stats')
         //.addRequired(true)
         .addChoices(
-          { name: 'Ana', value: 'hero_ana' },
+          		{ name: 'Ana', value: 'hero_ana' },
   				{ name: 'Baptiste', value: 'hero_baptiste' },
   				{ name: 'Brigitte', value: 'hero_brigitte' },
   				{ name: 'Kiriko', value: 'hero_kiriko' },
@@ -45,85 +45,129 @@ module.exports = {
 	async execute(client, interaction) {
    const value =  interaction.options.getString('character');
     switch (value) {
-        case 'ability_ana':
+        case 'hero_ana':
         const anaEmbed = new EmbedBuilder()
   			.setColor("#4a4c4e")
-  			.setTitle("You're powered up, get in there.")
+  			.setTitle("One day I'll end up in Fiji")
   			.addFields(
-        { name: 'Damage/healing with rifle', value: `75 damage/healing over 0.58 seconds`, inline: true },
-        { name: 'Rate of Fire', value: `0.8 seconds between shots`, inline: true },
-        { name: 'Ammo', value: `15 shots in her biotic rifle`, inline: true },
-        { name: 'Projectile speed', value: `125 meters per second unscoped`, inline: true },
-        { name: 'Aim type', value: `Unscoped: Projectile\nScoped: Hitscan`, inline: true },
+        { name: '__Health:__', value: `200 health points`, inline: true },
+        { name: '__Damage/healing with rifle:__', value: `75 damage/healing over 0.58 seconds`, inline: true },
+        { name: '__Rate of Fire:__', value: `0.8 seconds between shots`, inline: true },
+        { name: '__Ammo:__', value: `15 shots in her biotic rifle`, inline: true },
+        { name: '__Reload time:__', value: `1.5 seconds from start to finish`, inline: true },
+        { name: '__Aim type:__', value: `Unscoped: Projectile\nScoped: Hitscan`, inline: true },
             )
   			.setThumbnail(
   				"https://static.wikia.nocookie.net/overwatch_gamepedia/images/7/76/Ana.png/revision/latest/scale-to-width-down/143?cb=20160715094551"
   			)
         interaction.reply({ embeds: [anaEmbed]});
       break;
-      case 'hero_ana':
-        const heroAnaEmbed = new EmbedBuilder()
+      case 'ability_ana':
+        const abilityAnaEmbed = new EmbedBuilder()
   			.setColor("#4a4c4e")
   			.setTitle("You're powered up, get in there.")
   			.addFields(
-        { name: 'Testing', value: `75 damage/healing over 0.58 seconds`, inline: true },
-        { name: 'Rate of Fire', value: `0.8 seconds between shots`, inline: true },
-        { name: 'Ammo', value: `15 shots in her biotic rifle`, inline: true },
-        { name: 'Projectile speed', value: `125 meters per second unscoped`, inline: true },
-        { name: 'Aim type', value: `Unscoped: Projectile\nScoped: Hitscan`, inline: true },
+        { name: '__Sleep Dart:__', value: `Damage: 5\nDuration: 1.5-5 seconds\nCooldown: 14 seconds`, inline: true },
+        { name: '__Biotic Grenade:__', value: `Damage: 60\nHealing: 100\nDuration: 4 seconds\nCooldown: 10 seconds`, inline: true },
+        { name: '__Nano Boost:__', value: `Damage: 50% increase\nHealing: 250\nDamage reduction: 50%\nDuration: 8 seconds\nUltimate cost: 2100 points`, inline: true },
             )
   			.setThumbnail(
   				"https://static.wikia.nocookie.net/overwatch_gamepedia/images/7/76/Ana.png/revision/latest/scale-to-width-down/143?cb=20160715094551"
   			)
-        interaction.reply({ embeds: [heroAnaEmbed]});
+        interaction.reply({ embeds: [abilityAnaEmbed]});
       break;
-      case 'ability_baptiste':
+      case 'hero_baptiste':
         const baptisteEmbed = new EmbedBuilder()
   			.setColor("#4a4c4e")
-  			.setTitle("The lore of Jean-Baptiste Augustin")
-  			.setDescription(
-  				"Jean-Baptiste Augustin is also known as Baptiste. He is a combat medic and a former member of the Haitian Talon cell. He was orphaned at a young age and found himself living in the slums of Port-au-Prince. He is a former soldier who became disillusioned with the direction his country was heading and joined Talon as a way to make a difference, using his skills as a medic to help those in need. He developed a powerful exo-suit that gives him the ability to jump higher, move faster, and even deploy a device that creates a healing field for himself and his allies. He also carries a weapon that can fire a three-round-burst that deals damage to enemies. His history is complex and morally ambiguos, he believed that he is doing the right thing joining Talon, but eventually he realized that his actions are causing more harm than good, and starts to question his alliance with the organization."
-  			)
+  			.setTitle("Another day, another battlefield")
+  			.addFields(
+		{ name: '__Health:__', value: `200 health points`, inline: true },
+        { name: '__Damage/healing:__', value: `Primary: 7.2-25 damage per round\nSecondary: 70 (direct hit) or 50 (explosion)`, inline: true },
+        { name: '__Rate of Fire of Primary/Secondary:__', value: `Primary: 1 burst every 0.588 seconds\nSecondary: 1 shot every 0.9 seconds`, inline: true },
+        { name: '__Ammo:__', value: `Primary: 45\nSecondary: 13`, inline: true },
+        { name: '__Reload time:__', value: `1.5 seconds from start to finish with primary and secondary`, inline: true },
+        { name: '__Aim type:__', value: `Primary: Hitscan\nSecondary: Arching Area of Affect`, inline: true },
+            )
   			.setThumbnail(
   				"https://static.wikia.nocookie.net/overwatch_gamepedia/images/0/01/Baptiste-portrait.png/revision/latest/scale-to-width-down/162?cb=20190227081553"
   			)
         interaction.reply({ embeds: [baptisteEmbed]});
       break;
-      case 'ability_brigitte':
+      case 'ability_baptiste':
+        const abilityBaptisteEmbed = new EmbedBuilder()
+  			.setColor("#4a4c4e")
+  			.setTitle("Light them up!")
+  			.addFields(
+        { name: '__Regenerative Bust:__', value: `Healing: 50HP over 5 seconds plus initial 50-100 HP\nArea of Effect: 10 meter radius\nCooldown: 15 seconds`, inline: true },
+        { name: '__Immortality Field:__', value: `Durability of Ability: 150 HP\nArea of Effect: 6.5 meters radius\nDuration: 5 seconds\nCooldown: 25 seconds`, inline: true },
+        { name: '__Amplification Matrix:__', value: `Damage: 100% increase\nHealing: 100% increase\nMax Range: 35 meters\nDuration: 10 seconds\nUltimate cost: 2310 points`, inline: true },
+            )
+  			.setThumbnail(
+  				"https://static.wikia.nocookie.net/overwatch_gamepedia/images/0/01/Baptiste-portrait.png/revision/latest/scale-to-width-down/162?cb=20190227081553"
+  			)
+        interaction.reply({ embeds: [abilityBaptisteEmbed]});
+      break;
+      case 'hero_brigitte':
         const brigitteEmbed = new EmbedBuilder()
   			.setColor("#4a4c4e")
-  			.setTitle("The lore of Brigitte Lindholm")
-  			.setDescription(
-  				"Brigitte Lindholm is a playable character in the popular video game Overwatch. She is the daughter of Torbjorn and squire of Reinhardt, two of the original members of the Overwatch. She grew up listening to her father's stories about the heroes of Overwatch and dreamed of one day becoming a hero herself. She learned how to fight and build armor from her father and after Reinhardt saved her father's life, she decided to become his squire and followed him on his adventures. She is a strong and determined individual, with a deep sense of justice and sense of duty, she's also a very skilled engineer and is dedicated to protecting her allies."
-  			)
+  			.setTitle("Breaking me down just builds me up.")
+  			.addFields(
+		{ name: '__Health/armor:__', value: `Health: 150\nArmor: 50`, inline: true },
+        { name: '__Damage/healing:__', value: `Damage: 35 per swing\nHealing: 15 HP per second`, inline: true },
+        { name: '__Rate of Fire:__', value: `1 swing per 0.6 seconds`, inline: true },
+        { name: '__Max Range:__', value: `6 meters`, inline: true },
+        { name: '__Duration of Heals:__', value: `5 seconds `, inline: true },
+        { name: '__Aim type:__', value: `Melee`, inline: true },
+            )
   			.setThumbnail(
   				"https://static.wikia.nocookie.net/overwatch_gamepedia/images/8/8a/Brigitte_Concept.png/revision/latest/scale-to-width-down/255?cb=20180228195511"
   			)
         interaction.reply({ embeds: [brigitteEmbed]});
       break;
-      case 'ability_kiriko':
+      case 'ability_brigitte':
+        const abilityBrigitteEmbed = new EmbedBuilder()
+  			.setColor("#4a4c4e")
+  			.setTitle("Rally to me!")
+  			.addFields(
+        { name: '__Repair Pack:__', value: `Healing: 110HP over 2 seconds\nAmmo: 3\nDuration: 2 seconds\nCooldown: 6 seconds per charge`, inline: true },
+        { name: '__Whip Shot:__', value: `Damage: 70\nArea of Effect: 0.5 meters radius\nMax range: 20 meters\nCooldown: 4 seconds`, inline: true },
+        { name: '__Rally:__', value: `Healing: 15 overhealth per 0.5 seconds\nArea of Effect: 8.5 meters radius\nDuration: 10 seconds \nUltimate cost: 2800 points`, inline: true },
+            )
+  			.setThumbnail(
+  				"https://static.wikia.nocookie.net/overwatch_gamepedia/images/8/8a/Brigitte_Concept.png/revision/latest/scale-to-width-down/255?cb=20180228195511"
+  			)
+        interaction.reply({ embeds: [abilityBrigitteEmbed]});
+      break;
+	  case 'hero_kiriko':
         const kirikoEmbed = new EmbedBuilder()
   			.setColor("#4a4c4e")
-  			.setTitle("The lore of Kiriko Yamagami")
-  			.setDescription(
-  				`Kiriko Yamagami was raised by her grandmother and mother in Kanezaka. A highly spiritual woman, her grandmother's spiritual beliefs strongly influenced Kiriko, and shaped her fighting style. Kiriko's mother was a fierce ninja, who believed in action, standing up for oneself, and fighting for what was right. Training quietened her mind, and opened her to the guidance of the Fox Spirit, while her bond with the spirit allowed Kiriko to protect those who couldn't defend themselves. As such, she followed both paths. Kiriko's mother trained Genji and Hanzo Shimada in the way of the sword, as well as other ninja skills, such as how to wall climb. As such, Kiriko both grew up and learned alongside the duo, becoming a sort of "cute, little niece figure" to them.`
-  			)
+  			.setTitle("I'll find my own path.")
+  			.addFields(
+		{ name: '__Health:__', value: `Health: 200`, inline: true },
+        { name: '__Damage/healing:__', value: `Damage: 40 per Kunai\nHealing: 13 per tailsman or 26 per pair`, inline: true },
+        { name: '__Headshot:__', value: `Kunai headshots do 120 damage`, inline: true },
+        { name: '__Ammo:__', value: `10 shots for Healing Ofunda and 15 for Kunai`, inline: true },
+        { name: '__Healing per second:__', value: `78 HP `, inline: true },
+        { name: '__Aim type:__', value: `Projectile`, inline: true },
+            )
   			.setThumbnail(
   				"https://static.wikia.nocookie.net/overwatch_gamepedia/images/d/d4/Kiriko-portrait.png/revision/latest/scale-to-width-down/293?cb=20221114213127"
   			)
         interaction.reply({ embeds: [kirikoEmbed]});
       break;
-      case 'ability_lucio':
-        const lucioEmbed = new EmbedBuilder()
+      case 'ability_kiriko':
+        const abilityKirikoEmbed = new EmbedBuilder()
   			.setColor("#4a4c4e")
-  			.setTitle("The lore of Lucio Correia dos Santos")
-  			.setDescription(
-				`Lucio Correia dos Santos is a playable character in the popular video game Overwatch. He is a Brazilian DJ and freedom fighter who rose to fame as a musician, using his songs as a platform to spread messages of hope and inspiration. He has the ability to manipulate sound waves, both as a weapon and to provide speed boosts and healing to his allies. He also uses a Soundwave ability, which knocks back enemies, and an ultimate ability called "Sound Barrier" that provides a massive shield to nearby allies. Lucio grew up in the favelas of Rio de Janeiro and witnessed first hand the struggles of the people there, this led him to become a voice for the oppressed and using his music as a way of uplifting them and bringing change. He is an optimistic and charismatic person who wants to make the world a better place and help others. He's a popular hero among the players and is known for his energetic playstyle and flashy abilities.`
-  			)
+  			.setTitle("Let the Kitsune guide you!")
+  			.addFields(
+        { name: '__Swift Step:__', value: `Max range: 35 meters\nDuration: 0.25 seconds of invincibilty\nCooldown: 7 seconds`, inline: true },
+        { name: '__Protection Suzu:__', value: `Durability of Ability: 0.85\nArea of Effect: 6.5 meters radius\nDuration: 5 seconds\nCooldown: 25 seconds`, inline: true },
+        { name: '__Kitsune Rush:__', value: `Damage: 100% increase\nHealing: 100% increase\nMax Range: 35 meters\nDuration: 10 seconds \nUltimate cost: 2800 points`, inline: true },
+            )
   			.setThumbnail(
-  				"https://static.wikia.nocookie.net/overwatch_gamepedia/images/8/8c/Lucio-portrait.png/revision/latest/scale-to-width-down/189?cb=20160620004212"
+  				"https://static.wikia.nocookie.net/overwatch_gamepedia/images/d/d4/Kiriko-portrait.png/revision/latest/scale-to-width-down/293?cb=20221114213127"
   			)
-        interaction.reply({ embeds: [lucioEmbed]});
+        interaction.reply({ embeds: [abilityKirikoEmbed]});
       break;
       case 'ability_mercy':
         const mercyEmbed = new EmbedBuilder()
